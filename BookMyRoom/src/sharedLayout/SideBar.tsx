1d0e371580c18dd88ae5bc18ca7db17409c1b2fe
Sasa Nedic <svetasmak0@gmail.com>
2024-07-09 15:27:36 +0200

    Create SideBar component

---
+++ BookMyRoom/src/sharedLayout/SideBar.tsx
@@ -0,0 +1,13 @@
+const SideBar = () => {
+    return(
+        <div className="SideBar-container">
+            <ul>
+                <li><button>Rooms</button></li>
+                <li><button>Booking</button></li>
+                <li><button>Users</button></li>
+            </ul>
+        </div>
+    )
+}
+
+export default SideBar;
