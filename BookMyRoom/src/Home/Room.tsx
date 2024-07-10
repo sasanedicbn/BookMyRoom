941d3283bd7720cfd5b1c355edd4e7e408b2044f
Sasa Nedic <svetasmak0@gmail.com>
2024-07-10 02:04:48 +0200

    Add btn and create style for that

--- BookMyRoom/src/Home/Room.tsx
+++ BookMyRoom/src/Home/Room.tsx
@@ -1,6 +1,6 @@
 
 const Room = ({ room }) => {
-  const { id, name, description, maxCapacity, regularPrice, image } = room;
+  const { id, name, description, maxCapacity, regularPrice, image, created_at } = room;
 
   return (
     <li key={id} className="room-container">
@@ -8,8 +8,10 @@ const Room = ({ room }) => {
       {/* <h2>{name}</h2> */}
       <div className="room-details">
        <p className="room-details-description">{description}</p>
-       <p className="room-details-capacity">Max Capacity: {maxCapacity}</p>
+       <p className="room-details-capacity">Max Capacity: {maxCapacity} people</p>
        <p className="room-details-price">Regular Price: ${regularPrice}</p>
+       <p className="room-details-created">Posted: {created_at}</p>
+       <button className="btn-show-more">Show more...</button>
       </div>
     </li>
   );
