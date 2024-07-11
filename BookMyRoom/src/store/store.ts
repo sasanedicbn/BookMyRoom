e97f37c777af01b322b260b87c0d61cbc0853d6b
Sasa Nedic <svetasmak0@gmail.com>
2024-07-11 17:34:22 +0200

    Add modalSlice in store

--- BookMyRoom/src/store/store.ts
+++ BookMyRoom/src/store/store.ts
@@ -2,9 +2,11 @@
 
 import { configureStore } from '@reduxjs/toolkit';
 import roomsSlice from './roomsSlice';
+import modalSlice from './modalSlice';
 
 export const store = configureStore({
   reducer: {
     rooms: roomsSlice,
+    modals: modalSlice,
   },
 });
