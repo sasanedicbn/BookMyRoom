83a84cb629028ac06fd4d7fe24d909792a015e82
Sasa Nedic <svetasmak0@gmail.com>
2024-07-13 16:03:32 +0200

    Create newEditRoom validation

--- BookMyRoom/src/Home/Room/newEditRoom.tsx
+++ BookMyRoom/src/Home/Room/newEditRoom.tsx
@@ -1,16 +1,46 @@
-const NewEditRoom = ({handleCloseEditModal}) => {
-    const closeEditModal = () => {
-        handleCloseEditModal()
-    }
-    return (
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const schema = z.object({
+  name: z.string().nonempty("Cabin name is required"),
+  maxCapacity: z.number().positive("Must be greater than 0"),
+  regularPrice: z.number().positive("Must be greater than 0"),
+  discount: z.number().optional(),
+  description: z.string().nonempty("Description is required"),
+  photo: z.any().refine(file => file && file.length > 0, "Photo is required"),
+});
+
+const NewEditRoom = ({ handleCloseEditModal }) => {
+  const { register, handleSubmit, reset, formState: { isValid } } = useForm({
+    resolver: zodResolver(schema),
+    mode: "onChange",
+  });
+
+  const closeEditModal = () => {
+    handleCloseEditModal();
+  };
+
+  const onSubmit = (data) => {
+    console.log(data);
+    handleCloseEditModal();
+  };
+
+  useEffect(() => {
+    reset();
+  }, [reset]);
+
+
+  return (
     <>
-      <form className="create-cabin-form">
+      <form className="create-cabin-form" onSubmit={handleSubmit(onSubmit)}>
         <div className="form-group">
           <label htmlFor="name">Cabin name</label>
           <input
             type="text"
             id="name"
-            required
+            {...register("name")}
           />
         </div>
         <div className="form-group">
@@ -18,7 +48,7 @@ const NewEditRoom = ({handleCloseEditModal}) => {
           <input
             type="number"
             id="maxCapacity"
-            required
+            {...register("maxCapacity", { valueAsNumber: true })}
           />
         </div>
         <div className="form-group">
@@ -26,7 +56,7 @@ const NewEditRoom = ({handleCloseEditModal}) => {
           <input
             type="number"
             id="regularPrice"
-            required
+            {...register("regularPrice", { valueAsNumber: true })}
           />
         </div>
         <div className="form-group">
@@ -34,13 +64,14 @@ const NewEditRoom = ({handleCloseEditModal}) => {
           <input
             type="number"
             id="discount"
+            {...register("discount", { valueAsNumber: true })}
           />
         </div>
         <div className="form-group">
           <label htmlFor="description">Description for website</label>
           <textarea
             id="description"
-            required
+            {...register("description")}
           ></textarea>
         </div>
         <div className="form-group">
@@ -48,18 +79,17 @@ const NewEditRoom = ({handleCloseEditModal}) => {
           <input
             type="file"
             id="photo"
-            required
+            {...register("photo")}
           />
         </div>
         <div className="form-actions">
-          <button type="button" onClick={() => closeEditModal()}>Cancel</button>
-          <button type="submit">Create new cabin</button>
+          <button type="button" onClick={closeEditModal}>Cancel</button>
+          <button type="submit" disabled={!isValid} className={!isValid ? 'disabled-button' : ''}>Create new cabin</button>
         </div>
       </form>
       <div className="overlay"></div>
-      </>
-    );
-  };
-  
-  export default NewEditRoom;
-  
+    </>
+  );
+};
+
+export default NewEditRoom;
