49c6522f5cdde90ff5325926f25784f171a3c2c2
Sasa Nedic <svetasmak0@gmail.com>
2024-07-15 18:22:04 +0200

    Create some better code

--- BookMyRoom/src/Home/Room/newEditRoom.tsx
+++ BookMyRoom/src/Home/Room/newEditRoom.tsx
@@ -5,40 +5,59 @@ import * as z from 'zod';
 import { useSelector } from 'react-redux';
 
 const schema = z.object({
-  name: z.string(),
-  maxCapacity: z.number().positive(),
-  regularPrice: z.number().positive(),
+  name: z.string().min(1, "Name is required"),
+  maxCapacity: z.number().positive("Must be a positive number"),
+  regularPrice: z.number().positive("Must be a positive number"),
   discount: z.number().optional(),
-  description: z.string(),
+  description: z.string().min(1, "Description is required"),
   photo: z.string().optional()
 });
 
-const NewEditRoom = ({handleCloseEditModal, handleEditSubmit}) => {
+const NewEditRoom = ({ handleCloseEditModal, handleEditSubmit }) => {
   const currentRoom = useSelector((state) => state.rooms.currentRoom);
+  const { isEditModalOpen } = useSelector((state) => state.modals);
+
   console.log('currentRoom:', currentRoom);
+  console.log('isEditModalOpen:', isEditModalOpen);
 
   const {
     register,
     handleSubmit,
-    formState: { isValid },
+    formState: { isValid, errors },
     reset,
   } = useForm({
     resolver: zodResolver(schema),
     mode: 'onChange',
-    defaultValues: {
-      name: currentRoom.name || '',
-      maxCapacity: currentRoom.maxCapacity || '',
-      regularPrice: currentRoom.regularPrice || '',
-      discount: currentRoom.discount || '',
-      description: currentRoom.description || '',
-      photo: currentRoom.photo || '',
+    defaultValues: isEditModalOpen ? currentRoom : {
+      name: '',
+      maxCapacity: '',
+      regularPrice: '',
+      discount: '',
+      description: '',
+      photo: '',
     },
   });
 
   useEffect(() => {
     console.log('Resetting form with:', currentRoom);
-    reset(currentRoom);
-  }, [currentRoom, reset]);
+    if (isEditModalOpen) {
+      reset(currentRoom);
+    } else {
+      reset({
+        name: '',
+        maxCapacity: '',
+        regularPrice: '',
+        discount: '',
+        description: '',
+        photo: '',
+      });
+    }
+  }, [currentRoom, isEditModalOpen, reset]);
+
+  useEffect(() => {
+    console.log('isValid:', isValid);
+    console.log('errors:', errors);
+  }, [isValid, errors]);
 
   const onSubmit = (data) => {
     console.log('sasa');
@@ -52,31 +71,64 @@ const NewEditRoom = ({handleCloseEditModal, handleEditSubmit}) => {
       <form className="create-cabin-form" onSubmit={handleSubmit(onSubmit)}>
         <div className="form-group">
           <label htmlFor="name">Cabin name</label>
-          <input type="text" id="name" {...register('name')} />
+          <input
+            type="text"
+            id="name"
+            {...register('name')}
+          />
+          {errors.name && <span>{errors.name.message}</span>}
         </div>
         <div className="form-group">
           <label htmlFor="maxCapacity">Maximum capacity</label>
-          <input type="number" id="maxCapacity" {...register('maxCapacity')} />
+          <input
+            type="number"
+            id="maxCapacity"
+            {...register('maxCapacity')}
+          />
+          {errors.maxCapacity && <span>{errors.maxCapacity.message}</span>}
         </div>
         <div className="form-group">
           <label htmlFor="regularPrice">Regular price</label>
-          <input type="number" id="regularPrice" {...register('regularPrice')} />
+          <input
+            type="number"
+            id="regularPrice"
+            {...register('regularPrice')}
+          />
+          {errors.regularPrice && <span>{errors.regularPrice.message}</span>}
         </div>
         <div className="form-group">
           <label htmlFor="discount">Discount</label>
-          <input type="number" id="discount" {...register('discount')} />
+          <input
+            type="number"
+            id="discount"
+            {...register('discount')}
+          />
         </div>
         <div className="form-group">
           <label htmlFor="description">Description for website</label>
-          <textarea id="description" {...register('description')}></textarea>
+          <textarea
+            id="description"
+            {...register('description')}
+          ></textarea>
+          {errors.description && <span>{errors.description.message}</span>}
         </div>
         <div className="form-group">
           <label htmlFor="photo">Cabin photo</label>
-          <input type="file" id="photo" {...register('photo')} />
+          <input
+            type="file"
+            id="photo"
+            {...register('photo')}
+          />
         </div>
         <div className="form-actions">
           <button type="button" onClick={handleCloseEditModal}>Cancel</button>
-          <button type="submit" disabled={!isValid} className={isValid ? 'green' : 'grey'}>Save</button>
+          <button 
+            type="submit" 
+            disabled={!isValid} 
+            className={isValid ? 'green' : 'grey'}
+          >
+            Save
+          </button>
         </div>
       </form>
       <div className="overlay"></div>
