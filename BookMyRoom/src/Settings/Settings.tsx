a2c08ae48b27ff1e644daa8a877cec8015ae7561
sasanedicbn <svetasmak0@gmail.com>
2024-07-30 20:46:03 +0200

    setting added

---
+++ BookMyRoom/src/Settings/Settings.tsx
@@ -0,0 +1,27 @@
+const Settings = () => {
+    return(
+       <div className="settings-container">
+         <h2>Update hotel settings</h2>
+         <div>
+            <div className="settings-inputs">
+              <p>Minimum nights/booking</p> 
+              <input type="text"/>
+            </div>
+            <div className="settings-inputs">
+              <p>Maximum nights/booking</p> 
+              <input type="text"/>
+            </div>
+            <div className="settings-inputs">
+              <p>Maximum guests/booking</p> 
+              <input type="text"/>
+            </div>
+            <div className="settings-inputs">
+              <p>Breakfast price</p> 
+              <input type="text"/>
+            </div>
+         </div>
+       </div>
+    )
+}
+
+export default Settings
