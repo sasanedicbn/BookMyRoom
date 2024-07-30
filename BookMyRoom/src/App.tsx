3b8735a6071e1bfeb0984d7536c2185c2804fe86
sasanedicbn <andjelamokvica@gmail.com>
2024-07-30 20:36:52 +0200

    app

--- BookMyRoom/src/App.tsx
+++ BookMyRoom/src/App.tsx
@@ -1,29 +1,41 @@
 import './App.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import SideBar from './sharedLayout/SideBar';
+import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom';
 import Content from './Home/Content';
 import { store } from './store/store';
 import { Provider } from 'react-redux';
-import SingleRoomDetails from './Home/SingleRoomDetails';
-
+import { FaBed, FaHotel, FaCalendarCheck, FaUsers, FaCog } from 'react-icons/fa';
+import { Link, Outlet } from 'react-router-dom';
+import Settings from './Settings/Settings';
+import SideBar from './sharedLayout/SideBar';
+import ContentNavBar from './Home/ContentNavBar';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <SideBar />,
-    children: [{
-      path:'/',
-      element: <Content/>
-    }]
+    element: <SideBar/>,
+    children: [
+      {
+        path: '/',
+        element: <Navigate to="/rooms" replace />,
+      },
+      {
+        path: '/rooms',
+        element: <Content />,
+      },{
+        path: 'settings',
+        element:<Settings/>
+      }
+    ],
   },
 ]);
 
 function App() {
   return (
     <Provider store={store}>
-     <RouterProvider router={router} />
+      <RouterProvider router={router} />
     </Provider>
   );
 }
 
 export default App;
+
