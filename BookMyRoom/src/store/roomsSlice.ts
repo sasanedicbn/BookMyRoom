74a4202dab47561b204212e777dd7bb78bb64db5
sasanedicbn <svetasmak0@gmail.com>
2024-07-30 20:44:25 +0200

    roomsSlice

--- BookMyRoom/src/store/roomsSlice.ts
+++ BookMyRoom/src/store/roomsSlice.ts
@@ -1,29 +1,36 @@
-
 import { createSlice } from '@reduxjs/toolkit';
 
 export const roomsSlice = createSlice({
   name: 'rooms',
   initialState: {
-   rooms: [],
-   currentRoom: {
+    rooms: [],
+    // filteredRooms: [],
+    currentRoom: {
       name: '',
       maxCapacity: '',
       regularPrice: '',
       discount: '',
       description: '',
       image: '',
-   },
+    },
   },
   reducers: {
     getRooms: (state, action) => {
-        state.rooms = action.payload
+      state.rooms = action.payload;
+      // state.filteredRooms = action.payload; 
     },
-    singleRoom: (state,action) => {
-      state.currentRoom = action.payload
-    }
+    // setFilteredRooms: (state, action) => {
+    //   state.filteredRooms = action.payload;
+    // },
+    // setSortedRooms: (state, action) => {
+    //   state.filteredRooms = action.payload;
+    // },
+    setCurrentRoom: (state, action) => {
+      state.currentRoom = action.payload;
+    },
   },
 });
 
-export const { getRooms, singleRoom } = roomsSlice.actions;
+export const { getRooms,  setCurrentRoom } = roomsSlice.actions;
 
 export default roomsSlice.reducer;
