ab6bffe93ce67bfcbbd0e5e1c78d881baa2ff3ea
sasanedicbn <svetasmak0@gmail.com>
2024-07-30 20:43:41 +0200

    add sideBar

--- BookMyRoom/src/sharedLayout/SideBar.tsx
+++ BookMyRoom/src/sharedLayout/SideBar.tsx
@@ -1,17 +1,24 @@
-import { Outlet } from "react-router-dom";
-
+import { FaBed, FaCalendarCheck, FaCog, FaHotel, FaUsers } from "react-icons/fa";
+import { Link, Outlet } from "react-router-dom";
+import ContentNavBar from "../Home/ContentNavBar";
 
 const SideBar = () => {
   return (
     <div className="main-content">
-    <div className="SideBar-container">
-      <ul className="SideBar-ul">
-        <li><button>Rooms</button></li>
-        <li><button>Booking</button></li>
-        <li><button>Users</button></li>
-      </ul>
-    </div>
-    <Outlet/>
+      <div className="SideBar-container">
+        <span className="SideBar-icon"><FaBed /></span>
+        <p className="SideBar-title">BOOK MY ROOM</p>
+        <ul className="SideBar-ul">
+          <li><FaHotel /><Link to="/rooms">Rooms</Link></li>
+          <li><FaCalendarCheck /><Link to="/booking">Booking</Link></li>
+          <li><FaUsers /><Link to="/users">Users</Link></li>
+          <li><FaCog /><Link to="/settings">Settings</Link></li>
+        </ul>
+      </div>
+      <div className="main-content-data">
+       <ContentNavBar/>
+       <Outlet />
+       </div>
     </div>
   );
 }
