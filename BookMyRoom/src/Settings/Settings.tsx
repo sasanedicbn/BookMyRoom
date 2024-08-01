45dbed29efaf749ebe99b5884be61417914cf6f2
Sasa Nedic <svetasmak0@gmail.com>
2024-08-01 20:45:01 +0200

    Update settings component

--- BookMyRoom/src/Settings/Settings.tsx
+++ BookMyRoom/src/Settings/Settings.tsx
@@ -1,29 +1,56 @@
-const Settings = () => {
-    return(
-       <div className="main-settings">
-        <div className="settings-container">
-         <h2>Update hotel settings</h2>
-         <div>
-            <div className="settings-inputs">
-              <p>Minimum nights/booking</p> 
-              <input type="text"/>
-            </div>
-            <div className="settings-inputs">
-              <p>Maximum nights/booking</p> 
-              <input type="text"/>
-            </div>
-            <div className="settings-inputs">
-              <p>Maximum guests/booking</p> 
-              <input type="text"/>
-            </div>
-            <div className="settings-inputs">
-              <p>Breakfast price</p> 
-              <input type="text"/>
-            </div>
-          </div>
-         </div>
-       </div>
-    )
-}
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { updateSetting } from './settingsSlice';
 
-export default Settings
+const Settings = () => {
+  const dispatch = useDispatch();
+  const { minNight, maxNight, maxGuest, breakfastPrice } = useSelector((state) => state.settings);
+
+  const handleChange = (name, value) => {
+    dispatch(updateSetting({ name, value: Number(value) }));
+  };
+
+  return (
+    <div className="main-settings">
+      <div className="settings-container">
+        <h2>Update hotel settings</h2>
+        <div>
+          <div className="settings-inputs">
+            <p>Minimum nights/booking</p>
+            <input
+              type="text"
+              value={minNight}
+              onChange={(e) => handleChange('minNight', e.target.value)}
+            />
+          </div>
+          <div className="settings-inputs">
+            <p>Maximum nights/booking</p>
+            <input
+              type="text"
+              value={maxNight}
+              onChange={(e) => handleChange('maxNight', e.target.value)}
+            />
+          </div>
+          <div className="settings-inputs">
+            <p>Maximum guests/booking</p>
+            <input
+              type="text"
+              value={maxGuest}
+              onChange={(e) => handleChange('maxGuest', e.target.value)}
+            />
+          </div>
+          <div className="settings-inputs">
+            <p>Breakfast price</p>
+            <input
+              type="text"
+              value={breakfastPrice}
+              onChange={(e) => handleChange('breakfastPrice', e.target.value)}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Settings;
