7923b9c24cc7a4895e3392ecaca08a8ee05dd001
Sasa Nedic <svetasmak0@gmail.com>
2024-08-02 11:11:24 +0200

    Fix some error

--- BookMyRoom/src/Settings/Settings.tsx
+++ BookMyRoom/src/Settings/Settings.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { updateSetting } from './settingsSlice';
+import { updateSetting } from '../store/settingsSlice';
 
 const Settings = () => {
   const dispatch = useDispatch();
   const { minNight, maxNight, maxGuest, breakfastPrice } = useSelector((state) => state.settings);
+
 
   const handleChange = (name, value) => {
     dispatch(updateSetting({ name, value: Number(value) }));
--- BookMyRoom/src/store/store.ts
+++ BookMyRoom/src/store/store.ts
@@ -2,9 +2,11 @@
 
 import { configureStore } from '@reduxjs/toolkit';
 import roomsSlice from './roomsSlice';
+import settingsSlice from './settingsSlice';
 
 export const store = configureStore({
   reducer: {
     rooms: roomsSlice,
+    settings: settingsSlice,
   },
 });
