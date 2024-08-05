775f039c29429bc0b78df3e05c11550f9268756b
Sasa Nedic <svetasmak0@gmail.com>
2024-08-05 20:51:44 +0200

    Create Login component

---
+++ BookMyRoom/src/Login/Login.tsx
@@ -0,0 +1,21 @@
+const Login = () => {
+    return (
+      <div className="login-container">
+        <div className="login-form">
+          <h2>Login to your account</h2>
+          <div className="login-inputs">
+            <label htmlFor="email">Email address</label>
+            <input type="email" id="email" name="email" className="login-input" />
+          </div>
+          <div className="login-inputs">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" name="password" className="login-input" />
+          </div>
+          <button className="btn-login">Log in</button>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Login;
+  
