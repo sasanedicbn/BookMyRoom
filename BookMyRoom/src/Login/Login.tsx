8ca2177ffa0895369c2f1fc99f4e5ed6161eadf7
Sasa Nedic <svetasmak0@gmail.com>
2024-08-06 17:13:54 +0200

    Create navigate

--- BookMyRoom/src/Login/Login.tsx
+++ BookMyRoom/src/Login/Login.tsx
@@ -1,24 +1,63 @@
-import Logo from "../Home/Logo";
+import { useState } from 'react';
+import { toast } from 'react-toastify';
+import Logo from '../Home/Logo';
+import { supabase } from '../superbase/superbaseClient';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
-    return (
-      <div className="login-container">
-        <div className="login-form">
-          <Logo/>
-          <h2>Login to your account</h2>
-          <div className="login-inputs">
-            <label htmlFor="email">Email address</label>
-            <input type="email" id="email" name="email" className="login-input" />
-          </div>
-          <div className="login-inputs">
-            <label htmlFor="password">Password</label>
-            <input type="password" id="password" name="password" className="login-input" />
-          </div>
-          <button className="btn-login">Log in</button>
-        </div>
-      </div>
-    );
+  const navigate = useNavigate()
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async () => {
+    if (!email || !password) {
+      toast.error('All fields are required');
+      return;
+    }
+
+    try {
+      const { error } = await supabase.auth.signInWithPassword({ email, password });
+      if (error) throw error;
+      
+      toast.success('Logged in successfully');
+      navigate('/rooms')
+    } catch (error) {
+      console.error('Error logging in:', error);
+      toast.error(error.message);
+    }
   };
-  
-  export default Login;
-  
+
+  return (
+    <div className="login-container">
+      <div className="login-form">
+        <Logo/>
+        <h2>Login to your account</h2>
+        <div className="login-inputs">
+          <label htmlFor="email">Email address</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="login-input"
+          />
+        </div>
+        <div className="login-inputs">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="login-input"
+          />
+        </div>
+        <button className="btn-login" onClick={handleLogin}>Log in</button>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
