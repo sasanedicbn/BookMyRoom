13d077b29afea65600f82be8c331d2aaef1ee873
Sasa Nedic <svetasmak0@gmail.com>
2024-08-08 13:26:56 +0200

    Create Input component

--- BookMyRoom/src/UX/Input.tsx
+++ BookMyRoom/src/UX/Input.tsx
@@ -1,10 +1,16 @@
-// const Input = ({className,element}) => {
-//     return(
-//       <div className={className}>
-        
-//       </div>
-//     )
-// }
+const Input = ({className,labelText,text,name,value,onChange}) => {
+    return(
+      <div className={className}>
+        <label>{labelText}</label>
+        <input
+         type={text}
+         name={name}
+         value={value}
+         onChange={onChange}/>
+      </div>
+    )
+}
+ export default Input;
 
 // {/* <div className="settings-inputs">
 // <p>Minimum nights/booking</p>
