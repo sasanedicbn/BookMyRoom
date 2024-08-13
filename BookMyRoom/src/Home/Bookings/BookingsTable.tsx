74727d7622ea45d6b8aa0365516ee1289884cc46
Sasa Nedic <svetasmak0@gmail.com>
2024-08-13 22:41:16 +0200

    Add classes in components

--- BookMyRoom/src/Home/Bookings/BookingsTable.tsx
+++ BookMyRoom/src/Home/Bookings/BookingsTable.tsx
@@ -1,37 +1,25 @@
+import TableHead from "../TableHead";
+
 const BookingsTable = ({ bookings }) => {
     return (
-        <table style={tableStyle}>
-            
-            <tbody>
+        <table className="table">
+            <TableHead/>
+            <tbody className="table-body">
                 {bookings.map((booking, index) => (
                     <tr key={index}>
-                        <td style={tdStyle}>{booking.Bedrooms?.id}</td>
-                        <td style={tdStyle}>{booking.Guests?.fullName}</td>
-                        <td style={tdStyle}>{booking.Guests?.email}</td>
-                        <td style={tdStyle}>
+                        <td>{booking.Bedrooms?.id}</td>
+                        <td>{booking.Guests?.fullName}</td>
+                        <td>{booking.Guests?.email}</td>
+                        <td>
                             {new Date(booking.checkInDate).toLocaleDateString()} — {new Date(booking.checkOutDate).toLocaleDateString()}
                         </td>
-                        <td style={tdStyle}>{booking.status}</td>
-                        <td style={tdStyle}>${booking.totalPrice}</td>
+                        <td>{booking.status}</td>
+                        <td>${booking.totalPrice}</td>
                     </tr>
                 ))}
             </tbody>
         </table>
     );
 }
-
-const tableStyle = {
-    width: '100%',
-    borderCollapse: 'collapse',
-    margin: '20px 0',
-    fontSize: '18px',
-    textAlign: 'left'
-};
-
-
-const tdStyle = {
-    padding: '12px 15px',
-    borderBottom: '1px solid #dddddd'
-};
 
 export default BookingsTable;
--- BookMyRoom/src/Home/Bookings/TableHead.tsx
+++ BookMyRoom/src/Home/Bookings/TableHead.tsx
@@ -1,15 +1,16 @@
 const TableHead = () => {
     return(
-        <thead>
-        <tr className="table-head">
-            <th>Cabin</th>
-            <th>Guest</th>
-            <th>Email</th>
-            <th>Dates</th>
-            <th>Status</th>
-            <th>Amount</th>
-        </tr>
-    </thead>
-    )
+        <thead className="table-head">
+            <tr>
+                <th>Cabin</th>
+                <th>Guest</th>
+                <th>Email</th>
+                <th>Dates</th>
+                <th>Status</th>
+                <th>Amount</th>
+            </tr>
+        </thead>
+    );
 }
+
 export default TableHead;
