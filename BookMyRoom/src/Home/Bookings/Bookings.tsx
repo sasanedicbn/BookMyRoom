333542d9ed9e96aa215bc60a50042e25c5040f0f
Sasa Nedic <svetasmak0@gmail.com>
2024-08-13 17:04:20 +0200

    Change some function in Bookings

--- BookMyRoom/src/Home/Bookings/Bookings.tsx
+++ BookMyRoom/src/Home/Bookings/Bookings.tsx
@@ -1,12 +1,35 @@
+import { useState, useEffect } from "react";
 import ComponentWrapper from "../../UX/ComponentWrapper";
+import BookingsTable from "./BookingsTable";
+import { supabase } from "../../superbase/superbaseClient";
 
 const Bookings = () => {
-    return(
+    const [bookings, setBookings] = useState([]);
+
+    const getBookings = async () => {
+        let { data: bookings, error } = await supabase
+            .from('Bookings')
+            .select(`*,
+                Guests (name, email),
+                Cabins (cabinNumber)
+            `);
+        if (!error) {
+            setBookings(bookings);
+        } else {
+            console.error(error);
+        }
+    }
+
+    useEffect(() => {
+        getBookings();
+    }, []);
+
+    return (
         <ComponentWrapper>
-         <div>
-            BOOKINGS
-         </div>
+            <h2>BOOKINGS</h2>
+            <BookingsTable bookings={bookings} />
         </ComponentWrapper>
-    )
+    );
 }
+
 export default Bookings;
---
+++ BookMyRoom/src/Home/Bookings/BookingsTable.tsx
@@ -0,0 +1,8 @@
+const BookingsTable = ({booking}) => {
+    return(
+        <div>
+        {console.log(booking)}
+        </div>
+    )
+}
+export default BookingsTable;
