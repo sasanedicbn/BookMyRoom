6da5f8adcbef013ef6e13401d9154224befeaa7b
Sasa Nedic <svetasmak0@gmail.com>
2024-08-18 20:41:47 +0200

    Clean code

--- BookMyRoom/src/Pages/Bookings/Bookings.tsx
+++ BookMyRoom/src/Pages/Bookings/Bookings.tsx
@@ -10,7 +10,3 @@ const Bookings = () => {
 }
 
 export default Bookings;
-// ako je booking check-in => u modal treba da bude check-out, see-details  i delete,
-//ako je booking checked-out => u modal treba da bude see-details i delete,
-// -!- unconfermed => u modal treba da bude checked-in, see details i delete
-// isti modal kao i za kabine
