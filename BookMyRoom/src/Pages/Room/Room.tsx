e8c65fcf7faf05eb739c18e4393b926bbe62d7c9
Sasa Nedic <svetasmak0@gmail.com>
2024-08-22 18:23:16 +0200

    Create changes in Room component

--- BookMyRoom/src/Pages/Room/Room.tsx
+++ BookMyRoom/src/Pages/Room/Room.tsx
@@ -3,11 +3,16 @@ import { BiDotsVerticalRounded } from 'react-icons/bi';
 import Modals from '../../UX/Modals';
 import Button from '../../UX/Button';
 import NewEditRoom from './newEditRoom';
+import { FaEdit, FaTrash } from 'react-icons/fa';
+import { supabase } from '../../supabase/supabaseClient';
+import { useDispatch } from 'react-redux';
+import { getRooms } from '../../store/roomsSlice';
 
 const Room = ({ room }) => {
   const { name, maxCapacity, regularPrice, image, discount } = room;
   const [openMenuModal, setOpenMenuModal] = useState(false);
   const [openEditModal, setOpenEditModal] = useState(false);
+  const dispatch = useDispatch()
 
   const handleOpenMenuModal = () => {
     setOpenMenuModal(state => !state);
@@ -16,6 +21,38 @@ const Room = ({ room }) => {
   const handleOpenEditModal = () => {
     setOpenEditModal(true);
     setOpenMenuModal(false); 
+  };
+
+    // const handleDelete = async () => {
+    //     const { error } = await supabase
+    //       .from('Bedrooms')
+    //       .delete()
+    //       .eq('id', room.id);
+    
+    //     if (error) {
+    //       console.error('Error deleting row:', error);
+    //     } else {
+    //       console.log('Room deleted successfully');
+    //       const { data: rooms, error: fetchError } = await supabase.from('Bedrooms').select('*');
+    //       if (!fetchError) {
+    //         dispatch(getRooms(rooms));
+    //       }
+    //       setOpenMenuModal(false);
+    //     }
+    //   };
+
+
+  const modalsActions = {
+    'edit': {
+      icon: <FaEdit />,
+      onClick: handleOpenEditModal,
+      label: 'Edit'
+    },
+    'delete': {
+      icon: <FaTrash />,
+      onClick: handleDelete,
+      label: 'Delete'
+    },
   };
 
   return (
@@ -32,16 +69,14 @@ const Room = ({ room }) => {
       </tr>
 
       {openMenuModal && (
-        <Modals type="two-options">
-          <Button type="edit" onClick={handleOpenEditModal}>
-            Edit
-          </Button>
-          <Button type="delete" onClick={() => { /* implement delete logic */ }}>
-            Delete
-          </Button>
-          <Button type="secondary" onClick={handleOpenMenuModal}>
-            Cancel
-          </Button>
+        <Modals >
+          <div className={`options-menu two-options`}>
+          {Object.values(modalsActions).map(action => (
+            <Button key={action.label} type={action.label.toLowerCase()} onClick={action.onClick}>
+              {action.icon} {action.label}
+            </Button>
+          ))}
+          </div>
         </Modals>
       )}
 
