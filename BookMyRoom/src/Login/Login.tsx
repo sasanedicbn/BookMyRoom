545cc3044f2aa174660301db65dab04f03b28e3c
Sasa Nedic <svetasmak0@gmail.com>
2024-08-24 13:58:02 +0200

    Create style for OptionsBody component and add more code

--- BookMyRoom/src/Login/Login.tsx
+++ BookMyRoom/src/Login/Login.tsx
@@ -39,7 +39,7 @@ const Login = () => {
       navigate('/rooms');
     } catch (error) {
       console.error('Error logging in:', error);
-      toast.error(error.message);
+      toast.error(error?.message);
     }
   };
 
--- BookMyRoom/src/UX/OptionsMenu.tsx
+++ BookMyRoom/src/UX/OptionsMenu.tsx
@@ -13,34 +13,11 @@ interface OptionsMenuProps {
 //   { key: 'see-details', icon: <FaEye />, label: 'See Details', /* onClick: handleSeeDetails */ },
 // ];
 
- export const optionsAction = [
-  {key: 'edit', icon: <FaEdit/>, label: 'Edit'},
-  {key: 'delete', icon: <FaTrash/>, label: 'Delete'}
- ]
+
 
 const OptionsMenu: React.FC<OptionsMenuProps> = ({  modalsActions }) => {
   console.log('modalsActions', modalsActions)
   const modalType = modalsActions.length === 2 ? 'two-options' : 'three-options';
-  const handlerOpenEditModal = () => {
-    console.log('Open Edit Modal');
-  };
-
-  const handleDelete = () => {
-    console.log('Delete functionality here');
-  };
-
-  const handleCheckOut = () => {
-    console.log('Check-out functionality here');
-  };
-
-  const handleCheckIn = () => {
-    console.log('Check-in functionality here');
-  };
-
-  const handleSeeDetails = () => {
-    console.log('See details functionality here');
-  };
-
  
   return (
     <Modals type={modalType} >
