ae4edfdb4958f4dc061b5093444993396c00205f
Sasa Nedic <svetasmak0@gmail.com>
2024-08-25 15:07:40 +0200

    Add Spinner in RoomList component

--- BookMyRoom/src/Pages/Room/RoomsList.tsx
+++ BookMyRoom/src/Pages/Room/RoomsList.tsx
@@ -9,28 +9,23 @@ import Button from "../../UX/Button";
 import ComponentWrapper from "../../UX/ComponentWrapper";
 import TableHead from "./TableHead";
 import Modals from "../../UX/Modals";
-
+import Spinner from "../../global/Spinner";
 
 const RoomsList = () => {
   const dispatch = useDispatch();
   const [addNewRoom, setAddNewRoom] = useState(false);
-   const [openEditModal, setOpenEditModal] = useState(false);
+  const [openEditModal, setOpenEditModal] = useState(false);
 
   const rooms = useSelector((state) => state.rooms.rooms);
-  console.log('ROOMS', rooms)
+  console.log('ROOMS', rooms);
 
-  // export const optionsAction = [
-  //   {key: 'edit', icon: <FaEdit/>, label: 'Edit'},
-  //   {key: 'delete', icon: <FaTrash/>, label: 'Delete'}
-  //  ]
   const handleAddNewRoom = () => {
-    setAddNewRoom((state) => !state);
+    setAddNewRoom((prevState) => !prevState);
   };
 
   useEffect(() => {
     const roomsListHandler = async () => {
       const rooms = await fetchRooms();
-      console.log(rooms);
       if (rooms) {
         dispatch(getRooms(rooms));
       }
@@ -40,24 +35,31 @@ const RoomsList = () => {
 
   return (
     <ComponentWrapper type={'tableWrapper'}>
-    <div className="roomsList-container">
-      <SortRooms/>
-      <table className="rooms-table">
-        <TableHead/>
-        <tbody>
-          {rooms.map((room) => (
-            <Room key={room.id} room={room} />
-          ))}
-        </tbody>
-      </table>
-      <Button type={'success'} onClick={handleAddNewRoom}>
-        Add new room
-      </Button>
-      {addNewRoom &&
-      <Modals type={'addNewRoom'}>
-        <NewEditRoom setOpenEditModal={setOpenEditModal} closeMenuModal={handleAddNewRoom} />
-      </Modals>}
-    </div>
+      <div className="roomsList-container">
+        <SortRooms/>
+        {rooms.length ? (
+          <>
+            <table className="rooms-table">
+              <TableHead/>
+              <tbody>
+                {rooms.map((room) => (
+                  <Room key={room.id} room={room} />
+                ))}
+              </tbody>
+            </table>
+            <Button type={'success'} onClick={handleAddNewRoom}>
+              Add new room
+            </Button>
+            {addNewRoom && (
+              <Modals type={'addNewRoom'}>
+                <NewEditRoom setOpenEditModal={setOpenEditModal} closeMenuModal={handleAddNewRoom} />
+              </Modals>
+            )}
+          </>
+        ) : (
+          <Spinner/>
+        )}
+      </div>
     </ComponentWrapper>
   );
 };
