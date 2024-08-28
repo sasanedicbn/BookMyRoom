aa4f33e4e05f840e1cac428285704ad49f049b89
Sasa Nedic <svetasmak0@gmail.com>
2024-08-28 18:04:06 +0200

    Create updateHasBreakfast function

---
+++ BookMyRoom/src/api/updateBreakfast.ts
@@ -0,0 +1,16 @@
+import { supabase } from "../supabase/supabaseClient";
+
+const updateHasBreakfast = async (bookingId, hasBreakfast) => {
+    try {
+        const { error } = await supabase
+            .from('Bookings')
+            .update({ hasBreakfast })
+            .eq('id', bookingId);
+        
+        if (error) throw error;
+
+        console.log('Booking updated successfully');
+    } catch (error) {
+        console.error('Error updating booking:', error.message);
+    }
+};
