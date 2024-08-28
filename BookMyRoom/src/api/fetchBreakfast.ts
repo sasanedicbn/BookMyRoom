905bac3b7cdcb0b2684de29c60d974f6c1ed3d09
Sasa Nedic <svetasmak0@gmail.com>
2024-08-28 12:11:38 +0200

    Create fetchBreakfastSetting function

--- BookMyRoom/src/api/fetchBreakfast.ts
+++ BookMyRoom/src/api/fetchBreakfast.ts
@@ -1,4 +1,21 @@
-
+const fetchBreakfastSetting = async () => {
+    try {
+      const { data, error } = await supabase
+        .from('Settings')
+        .select('breakfast');
+  
+      if (error) {
+        console.error('Greška pri dohvaćanju podataka:', error);
+        return;
+      }
+  
+      console.log('Podaci:', data);
+      return data;
+    } catch (error) {
+      console.error('Došlo je do greške:', error);
+    }
+  };
+  
   
  
   
