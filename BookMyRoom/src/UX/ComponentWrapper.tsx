33ea023f78b7f211bf7469c023fdb1d22f95463a
Sasa Nedic <svetasmak0@gmail.com>
2024-08-30 14:44:20 +0200

    Create changes in ComponentWrapper and Input

--- BookMyRoom/src/UX/ComponentWrapper.tsx
+++ BookMyRoom/src/UX/ComponentWrapper.tsx
@@ -1,15 +1,23 @@
-const ComponentWrapper = ({type, children}) => {
+import React from 'react';
 
-    const wrapper = {
-        componentWrapper: 'base-container',
-        tableWrapper: 'table-wrapper'
-    }
-   
-    return(
-        <div className={wrapper[type]}>
-          {children}
-        </div>
-    )
+type ComponentType = 'componentWrapper' | 'tableWrapper';
+
+interface ComponentWrapperProps {
+  type: ComponentType;
+  children: React.ReactNode;
 }
 
-export default ComponentWrapper
+const ComponentWrapper: React.FC<ComponentWrapperProps> = ({ type, children }) => {
+  const wrapper: { [key in ComponentType]: string } = {
+    componentWrapper: 'base-container',
+    tableWrapper: 'table-wrapper'
+  };
+
+  return (
+    <div className={wrapper[type]}>
+      {children}
+    </div>
+  );
+};
+
+export default ComponentWrapper;
--- BookMyRoom/src/UX/Input.tsx
+++ BookMyRoom/src/UX/Input.tsx
@@ -1,12 +1,21 @@
-const Input = ({type,name,value,onChange}) => {
-    return(
-        <input
-         type={type}
-         name={name}
-         value={value}
-         onChange={onChange}
-         />
-    )
-}
- export default Input;
+import React from 'react';
 
+interface InputProps {
+  type: string;
+  name: string;
+  value: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const Input: React.FC<InputProps> = ({ type, name, value, onChange }) => {
+  return (
+    <input
+      type={type}
+      name={name}
+      value={value}
+      onChange={onChange}
+    />
+  );
+};
+
+export default Input;
