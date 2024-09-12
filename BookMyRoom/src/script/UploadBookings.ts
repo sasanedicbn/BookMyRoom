ca78da59fa9698831640390014356f55ecf4fcde
Sasa Nedic <svetasmak0@gmail.com>
2024-09-12 23:15:23 +0200

    Clean code

--- BookMyRoom/src/script/UploadBookings.ts
+++ BookMyRoom/src/script/UploadBookings.ts
@@ -1,20 +1,15 @@
 import { addBookingsData } from "../api/Booking/addBookings";
 import { deleteAllBookings } from "../api/Booking/deleteAllBookings";
 import { bookings } from "./data-Booking";
-import { dataGuest } from "./data-guests";
 
-// const bookingsToAdd = dataGuest.map((guest) => ({
-//   fullName: guest.fullName,
-//   email: guest.email,
-// }));
+
 
 export const uploadBookings = async () => {
   const removeBooking = await deleteAllBookings();
   if (removeBooking) {
     const success = await addBookingsData(bookings);
     if (success) {
-      //  addBookingsData(bookingsToAdd)
-      //  console.log('guest fullname and email',bookingsToAdd)
+      
         console.log('succes bookings', bookings)
       console.log('New bookings have been added successfully');
     } else {
